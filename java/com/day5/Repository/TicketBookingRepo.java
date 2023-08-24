7f6afb582dd40274d8d18a4244d519ca8e576620
monosiva <108830219+monosiva@users.noreply.github.com>
2023-08-24 09:50:22 +0530

    Delete java/com/day5 directory

--- java/com/day5/Repository/TicketBookingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.day5.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.day5.Model.TicketBookingModel;
-
-public interface TicketBookingRepo extends JpaRepository<TicketBookingModel, Integer> {
-
-}
